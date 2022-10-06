0b4aef3dad5e477a7527767ecd1ff6b05461ff4a
Kevin Bell <bellKevin@pm.me>
2022-10-06 08:36:14 -0600

    Update ch5studentGradesWithFiles.cpp

--- ch5studentGradesWithFiles/ch5studentGradesWithFiles/ch5studentGradesWithFiles.cpp
+++ ch5studentGradesWithFiles/ch5studentGradesWithFiles/ch5studentGradesWithFiles.cpp
@@ -7,15 +7,18 @@ using namespace std;
 int main()
 {
     string response="y", name;
-    double score, average;
+    double score1, score2, score3, average;
     cout << "Ch 5 Student Grades by Kevin Bell\n\n";
-    while (response == "y") {
+    while (response == "y" || response =="Y" || response == "Yes" || response == "YES" || response == "yes") {
         cout << "Enter student name: ";
         cin >> name;
         cout << "Enter three scores separated by a space: ";
-        cin >> score;
-        average = (score + score + score) / 3;
+        cin >> score1 >> score2 >> score3;
+        average = (score1 + score2 + score3) / 3;
         cout << "Student's average is " << average << endl;
-
+        cout << "Do you have another student to enter? (y/n) ";
+        cin >> response;
     }
+    system("pause");
+    return 0;
 }
