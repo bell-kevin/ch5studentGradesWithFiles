85b3a79a888996f652e9081a11cb96bb3ba053ad
Kevin Bell <bellKevin@pm.me>
2022-10-05 20:45:44 -0600

    Update ch5studentGradesWithFiles.cpp

--- ch5studentGradesWithFiles/ch5studentGradesWithFiles/ch5studentGradesWithFiles.cpp
+++ ch5studentGradesWithFiles/ch5studentGradesWithFiles/ch5studentGradesWithFiles.cpp
@@ -6,23 +6,14 @@ using namespace std;
 
 int main()
 {
-    string response="y";
+    string response="y", name;
     double score;
     cout << "Ch 5 Student Grades by Kevin Bell\n\n";
     while (response == "y") {
         cout << "Enter student name: ";
+        cin >> name;
         cout << "Enter three scores separated by a space: ";
         cin >> score;
+
     }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
